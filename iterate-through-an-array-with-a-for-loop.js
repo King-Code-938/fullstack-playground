dd75b2df897283ee6efb8bcc7e65ba37d290f5b3
Seizer <idowuenoch65@gmail.com>
2025-06-09 01:03:59 +0100

    JS basics

---
+++ iterate-through-an-array-with-a-for-loop.js
@@ -0,0 +1,13 @@
+** start of script.js **
+
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total = 0;
+for(let i=0; i<myArr.length; i++){
+  total += myArr[i];
+}
+
+** end of script.js **
+
